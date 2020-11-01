c6675d004c2428613743b7d460b1585445fe293c
Emeric D <emericdefay@gmail.com>
2020-11-02 00:07:13 +0100

    Adding some details

--- MAS2MBS.py
+++ MAS2MBS.py
@@ -14,22 +14,34 @@ import serialReaderMaker
 import serial
 
 def createScript(name="default.mid"):
-    print("Program launched :")
-    print("\tOpen : " + name)
-    midi_arduino.start(name)
-    print("\tFile : script_arduino.ino : Created on /arduino/script_arduino/")
-    print("Program done.\n")
-    return print("If the script_arduino.ino is to much for your arduino board, use createSerialMaker()")
+    try:
+        print("Program launched :")
+        print("\tOpen : " + name)
+        midi_arduino.start(name)
+        print("\tFile : script_arduino.ino : Created on /arduino/script_arduino/")
+        print("Program done.\n")
+        print("If the script_arduino.ino is to much for your arduino board, use createSerialMaker()")
+        pass
+    except FileNotFoundError:
+        print("The file {} doesn't exist.".format(name))
+        pass
 
 def createSerialMaker(name="default.mid",arduino_COM="COM3",warningMsg=True):
-    print("Using Serial method.")
+    print("Using Serial method :\n")
     serialReaderMaker.start()
     try:
         serialSender.start(name,arduino_COM,warningMsg)
+        pass
     except serial.serialutil.SerialException:
         print("Not the arduino_COM.")
+        pass
+    except KeyboardInterrupt:
+        print("Abort the program.")
+        pass
+    except FileNotFoundError:
+        print("The file {} doesn't exist.".format(name))
         pass
 
 if __name__ == '__main__':
     #createScript() #Create the script_arduino.ino
-    createSerialMaker("default.mid","COM3",False) # Use Serial Method, when storage of board is not enough.
+    createSerialMaker("default.mid","COM3",True) # Use Serial Method, when storage of board is not enough.
--- serialSender.py
+++ serialSender.py
@@ -27,11 +27,11 @@ def printingLap(liste=[1 for val in range(30)],arduino_COM="COM3",lap=1,allLaps=
                 attempt += 1
 
                 port_serie.write('2'.encode('utf-8'))
-                print("Launching lap n°{} - Waiting Arduino information - Request n°{}...".format(lap,attempt))
+                print("\tLaunching lap n°{} - Waiting Arduino information - Request n°{}...".format(lap,attempt))
 
                 time.sleep(1)
                 if b'9' in port_serie.readline(port_serie.inWaiting()):
-                    print("Start printing Lap n°{}".format(lap))
+                    print("\t\tStart printing Lap n°{}".format(lap))
 
                     while (b'4' not in port_serie.readline(port_serie.inWaiting())):
 
@@ -46,38 +46,40 @@ def printingLap(liste=[1 for val in range(30)],arduino_COM="COM3",lap=1,allLaps=
                                     port_serie.write('0'.encode('utf-8'))
 
                                 if note > len(liste):
-                                    print("Lap done : Waiting for next Lap...")
+                                    print("\tLap done : Waiting for next Lap...")
                                     break
 
                             if(b'1' in (port_serie.readline())):
-                                print("Lap n°{}/{}-[{:02};{}]".format(lap,allLaps,note+1,liste[note]))
+                                print("\t\tLap n°{}/{}-[{:02};{}]".format(lap,allLaps,note+1,liste[note]))
                                 note += 1
                             if(b'2' in (port_serie.readline()) or note == len(liste)):
                                 break
-
-                        note = 0
                         break
-
                     break
-    pass
+    print("")
 
 def start(name="default.mid",arduino_COM="COM3",warningMsg=True):
     """ name : MidiFileName.mid
     arduino_COM : The port of your arduino.
     warningMsg : False/True. If true, remember the user to upload 'serialReaderPrinter.ino' on arduino board. """
     if warningMsg:
-        print("Remember to upload 'serialReaderPrinter.ino' on your arduino board.")
-        print("This warning will stay 20 seconds. To make sure that you have the time to check.")
-        print("You can disable this reminder : start('MidiFileName.mid',False)")
-        time.sleep(20)
+        print("\n\tReminder : \n\t  Remember to upload 'serialReaderPrinter.ino' on your arduino board.")
+        print("\tThis warning will stay 20 seconds. To make sure that you have the time to check.")
+        print("\tYou can disable this reminder : start('MidiFileName.mid',ArduinoPortCOM,->False<-)")
+        print("\n\t[",end="")
+        for temp in range(40):
+            time.sleep(0.5)
+            print("#",end="")
+        print("]")
+
 
     liste = exploit.start(name,False)
-    print("Start to print : {} - Amount of laps : {}.".format(name,len(liste)))
-    print("Average time : {} min.".format(int((10*len(liste))/60)))
+    print("\n\tStart to print : {} - Amount of laps : {}.".format(name,len(liste)))
+    print("\tAverage time : {} min.\n".format(int((10*len(liste))/60)))
     time.sleep(2)
     for k in range(len(liste)):
         printingLap(liste[k],arduino_COM,k+1,len(liste))
-    Return ("Printing done.")
+    Return ("\nPrinting done.")
 
 if __name__ == '__main__':
     #Debugging
